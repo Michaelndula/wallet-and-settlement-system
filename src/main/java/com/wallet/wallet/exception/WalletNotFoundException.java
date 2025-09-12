1e7f6bfbf652b6614cbbb9711f248576edf50465
michael <michaelndula@gmail.com>
2025-09-12 19:56:19 +0300

    Adding custom error handling for Insufficient funds and duplicate transactions

--- src/main/java/com/wallet/wallet/exception/WalletNotFoundException.java
+++ src/main/java/com/wallet/wallet/exception/WalletNotFoundException.java
@@ -1,13 +1,9 @@
 package com.wallet.wallet.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
 /**
  * Exception thrown when a wallet is not found for a given ID.
  * Results in an HTTP 404 Not Found response.
  */
-@ResponseStatus(HttpStatus.NOT_FOUND)
 public class WalletNotFoundException extends RuntimeException {
     public WalletNotFoundException(String message) {
         super(message);
